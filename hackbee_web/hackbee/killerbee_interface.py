e6421c704ba5b72102cf783d0f2a536de04f6bf3
Vadim <vadim.muhametcin@gmx.de>
2022-02-25 21:08:49 +0100

    Initial commit

---
+++ hackbee_web/hackbee/killerbee_interface.py
@@ -0,0 +1,30 @@
+import os
+from killerbee import *
+
+def get_device_information():
+    device_information = killerbee.show_dev()
+    return device_information
+
+def get_open_channels(dev_id, channel, verbose=False, file_path=None, delay=2):
+    '''
+    Runs killerbees tool zbstumbler. 
+    Structure of network_data =
+    [{
+        'panid': '0x1234', 
+        'source': '0x0000', 
+        'extpanid': '00:xx:xx:xx:xx:xx:xx:xx', 
+        'stackprofile': <'Network Specific'|'ZigBee Enterprise'|'ZigBee Enterprise'>
+        'stackversion': <'ZigBee Prototype'|'ZigBee 2004'|'ZigBee 2006/2007'>
+        'channel': <11-25>
+    }]
+    TODO:
+    Add support for verbose by displaying tool output on web interface in realtime.
+    '''
+    if not dev_id:
+        return "Please provide Device ID/Interface."
+    elif not channel:
+        return "Please provide channel."
+    if verbose:
+        networkdata,txcount, rxcount = os.system("sudo zbstumbler -i {0} -c {1} -s {2} -w {3} -v".format(dev_id, channel, delay, file_path))
+    else:
+        networkdata,txcount, rxcount = os.system("sudo zbstumbler -i {0} -c {1} -s {2} -w {3}".format(dev_id, channel, delay, file_path))
