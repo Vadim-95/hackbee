6b9a315640b3b0fb4451477e8152eb4c8c4cc173
Vadim <vadim.muhametcin@gmx.de>
2022-02-25 21:31:57 +0100

    Add duration to zbstumbler
    As zbstumbler won't end without any interaction, we have added a duration in seconds.

--- hackbee_web/hackbee/killerbee_interface.py
+++ hackbee_web/hackbee/killerbee_interface.py
@@ -3,9 +3,10 @@ from killerbee import *
 
 def get_device_information():
     device_information = killerbee.show_dev()
+    print(device_information)
     return device_information
 
-def get_open_channels(dev_id, channel, verbose=False, file_path=None, delay=2):
+def get_open_channels(dev_id, channel, verbose=False, file_path=None, delay=2, scan_time=10):
     '''
     Runs killerbees tool zbstumbler. 
     Structure of network_data =
@@ -25,6 +26,9 @@ def get_open_channels(dev_id, channel, verbose=False, file_path=None, delay=2):
     elif not channel:
         return "Please provide channel."
     if verbose:
-        networkdata,txcount, rxcount = os.system("sudo zbstumbler -i {0} -c {1} -s {2} -w {3} -v".format(dev_id, channel, delay, file_path))
+        networkdata,txcount, rxcount = os.system("sudo zbstumbler -i {0} -c {1} -s {2} -w {3} -v -d {4}".format(dev_id, channel, delay, file_path, scan_time))
     else:
-        networkdata,txcount, rxcount = os.system("sudo zbstumbler -i {0} -c {1} -s {2} -w {3}".format(dev_id, channel, delay, file_path))
+        networkdata,txcount, rxcount = os.system("sudo zbstumbler -i {0} -c {1} -s {2} -w {3} -d {4}".format(dev_id, channel, delay, file_path, scan_time))
+    print(networkdata)
+    print(txcount)
+    print(rxcount)
--- hackbee_web/hackbee/views.py
+++ hackbee_web/hackbee/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import killerbee_interface as kb
 
-# Create your views here.
+def index(request):
+    kb.get_device_information()
+    kb.get_open_channels()
+    return HttpResponse(
+        "Hello"
+    )
