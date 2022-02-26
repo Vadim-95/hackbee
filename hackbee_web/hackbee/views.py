d1970de4ae2488bd4d957a7d477c23486a28b6be
Vadim <vadim.muhametcin@gmx.de>
2022-02-26 14:34:08 +0100

    Add html template for structuring the interface.

--- hackbee_web/hackbee/views.py
+++ hackbee_web/hackbee/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 from .killerbee_interface import *
 
 def index(request):
-    get_device_information()
-    #get_open_channels()
-    get_open_channels("1:3","15",scan_time=3)
-    return HttpResponse(
-        "Hello"
-    )
+    template = 'hackbee/index.html'
+    device_list = get_device_information()
+    # get_open_channels("1:3","15",scan_time=3)
+    context = {
+        'device_information': device_list,
+    }    
+    return render(request, template, context = context)
